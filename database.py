7b4babad69a7a51865d94f3cef3e973076bda05f
Yash Goyal <yashg4656@gmail.com>
2026-06-10 14:29:01 +0530

    Fix: Hidden MongoDB URI using environment variables

--- database.py
+++ database.py
@@ -1,16 +1,18 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-MONGO_URI ="mongodb+srv://admin:rGRyzTnt5BMWTxsQ@cluster0.juxrwe5.mongodb.net/?appName=Cluster0"
+# .env file ko load karne ke liye
+load_dotenv()
+
+# Environment variable se URI nikalna
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
-
 db = client["todo_db"]
 
 tasks_collection = db["tasks"]
-
 users_collection = db["users"]
-
-
 try:
     client.admin.command("ping")
     print("✅ MongoDB Connected Successfully!")
